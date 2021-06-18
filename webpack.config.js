323a9115f211261e4e3c14f5535273d16c187d4b
anjessen <antoinejessen19@gmail.com>
2021-06-18 03:09:07 +0200

    modal

--- webpack.config.js
+++ webpack.config.js
@@ -1,63 +1,59 @@
 const path = require("path");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
-const CopyWebpackPlugin = require("Copy-Webpack-Plugin");
-const {CleanWebpackPlugin} = require("clean-webpack-plugin");
-
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 
 module.exports = {
-    entry: {
-      main: path.join(__dirname, "src/index.js"),
-      form: path.join(__dirname, "src/form/form.js"),
-      topbar: path.join(__dirname, "src/form/form.js")
-    },
+  entry: {
+    main: path.join(__dirname, "src/index.js"),
+    form: path.join(__dirname, "src/form/form.js"),
+    topbar: path.join(__dirname, "src/assets/javascripts/topbar.js"),
+  },
   output: {
     path: path.join(__dirname, "dist"),
-    filename: "[name].bundle.js"
+    filename: "[name].bundle.js",
   },
   module: {
     rules: [
       {
         test: /\.js/,
         exclude: /(node_modules)/,
-        use: ["babel-loader"]
+        use: ["babel-loader"],
       },
       {
         test: /\.scss$/i,
-        use: ["style-loader", "css-loader", "sass-loader"]
-      }
-    ]
+        use: ["style-loader", "css-loader", "sass-loader"],
+      },
+    ],
   },
   plugins: [
     new CleanWebpackPlugin(),
     new CopyWebpackPlugin({
-        patterns: [
-            {
-                from: './src/assets/img/*',
-                to: './assets/img',
-                flatten: true,
-            },
-        ],
-
+      patterns: [
+        {
+          from: "./src/assets/images/*",
+          to: 'assets/images/[name][ext]',
+        },
+      ],
     }),
     new HtmlWebpackPlugin({
-      filename: "index.html",  
+      filename: "index.html",
       template: path.join(__dirname, "./src/index.html"),
-      chunks: ["main", "topbar"]
+      chunks: ["main", "topbar"],
     }),
     new HtmlWebpackPlugin({
-        filename: "form.html",
-        template: path.join(__dirname, "./src/form/form.html"),
-        chunks: ["form", "topbar"]
-      })
+      filename: "form.html",
+      template: path.join(__dirname, "./src/form/form.html"),
+      chunks: ["form", "topbar"],
+    }),
   ],
   stats: "minimal",
   devtool: "source-map",
   mode: "development",
   devServer: {
-    open: true,
-    contentBase: path.resolve(__dirname, "./dist"),
-    hot: true,
+    open: false,
+    contentBase: path.resolve(__dirname, './dist'),
     inline: true,
-    port: 4000
-  }
+    port: 4000,
+  },
 };
